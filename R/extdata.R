6da1ea2f305d5d17d3196d07b5eba78c8d8df15a
Diego Campos <camposd@email.chop.edu>
2018-09-11 21:37:38 -0400

    Adding R source code

---
+++ R/extdata.R
@@ -0,0 +1,106 @@
+#' BMI Anthro
+#'
+#' Part of default CDC-derived tables
+#'
+#' Contains BMI data for calculating BMI
+#'
+#' @name bmianthro
+#'
+#' @section bmianthro.txt:
+#'
+#' Used in function `cleangrowth()`
+#'
+#'
+NULL
+
+#' CDC Growth Percentile Table
+#'
+#' Part of default CDC-derived tables
+#'
+#' Contains percentiles for various ages, gender, and weights, pre-calculated by CDC
+#'
+#' @name growth_cdc_ext
+#'
+#' @section growthfile_cdc_ext.csv:
+#'
+#' Used in function `cleangrowth()`
+#'
+#'
+NULL
+
+#' Length to Age Table
+#'
+#' Part of default CDC-derived tables
+#'
+#' Contains percentiles for various ages, gender, and weights, pre-calculated by CDC
+#'
+#' @name lenanthro
+#'
+#' @section lenanthro.txt:
+#'
+#' Used in function `cleangrowth()`
+#'
+#'
+NULL
+
+#' Tanner Growth Velocity Table
+#'
+#' Part of default CDC-derived tables
+#'
+#' Contains velocities for growth pre-calculated by CDC
+#'
+#' @name tanner_ht_vel
+#'
+#' @section tanner_ht_vel.csv:
+#'
+#' Used in function `cleangrowth()`
+#'
+#'
+NULL
+
+#' Weight Anthro Table
+#'
+#' Part of default CDC-derived tables
+#'
+#' Contains median and standard deviation for weight by age and gender
+#'
+#' @name weianthro
+#'
+#' @section weianthro.csv:
+#'
+#' Used in function `cleangrowth()`
+#'
+#'
+NULL
+
+#' WHO Maximum Height Velocity for (3\eqn{\sigma})
+#'
+#' Part of default WHO-derived tables
+#'
+#' Contains three standard deviations for the World Health Organization values of
+#' maximum height velocities.
+#'
+#' @name who_ht_maxvel
+#'
+#' @section who_ht_maxvel_3sd.csv:
+#'
+#' Used in function `cleangrowth()`
+#'
+#'
+NULL
+
+#' WHO Height Velocity for (3\eqn{\sigma})
+#'
+#' Part of default WHO-derived tables
+#'
+#' Contains three standard deviations for the World Health Organization values of
+#' height velocities.
+#'
+#' @name who_ht_vel_3sd
+#'
+#' @section who_ht_vel_3sd.csv:
+#'
+#' Used in function `cleangrowth()`
+#'
+#'
+NULL
